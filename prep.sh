9a47901259f7bcefe8b4580bedd6a4b1a5b0d22e
Gaurav Chauhan <iitb.gaurav@gmail.com>
2016-02-29 17:26:45 -0800

    easier setup

---
+++ prep.sh
@@ -0,0 +1,8 @@
+cd ~/
+echo "Downloading setup files in ~/shell_setup"
+mkdir shell_setup
+cd shell_setup
+wget https://raw.githubusercontent.com/ImGauravC/dotfiles/master/shell_setup/util.py
+wget https://raw.githubusercontent.com/ImGauravC/dotfiles/master/shell_setup/bootstrap.py
+echo 'Usage: python bootstrap.py [-h]'
+
