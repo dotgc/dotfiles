167a4f17c5ce98699a8233b6cc694195ad10be72
Gaurav Chauhan <dotgc@users.noreply.github.com>
2016-03-24 02:28:03 +0530

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -2,7 +2,7 @@ cd ~/
 echo "Downloading setup files in ~/shell_setup"
 mkdir shell_setup
 cd shell_setup
-wget https://raw.githubusercontent.com/ImGauravC/dotfiles/master/shell_setup/util.py
-wget https://raw.githubusercontent.com/ImGauravC/dotfiles/master/shell_setup/bootstrap.py
+wget https://raw.githubusercontent.com/dotgc/dotfiles/master/shell_setup/util.py
+wget https://raw.githubusercontent.com/dotgc/dotfiles/master/shell_setup/bootstrap.py
 echo 'Usage: python ~/shell_setup/bootstrap.py [-h]'
 
