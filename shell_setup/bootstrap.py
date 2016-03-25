9064d8ba54bbef34a1cb1bd423a7670592791198
Gaurav Chauhan <dotgc@users.noreply.github.com>
2016-03-25 08:56:34 +0530

    move to backup instead of overwriting

--- shell_setup/bootstrap.py
+++ shell_setup/bootstrap.py
@@ -16,21 +16,29 @@ GIT_COMPLETION_URL = 'https://raw.githubusercontent.com/git/git/master/contrib/c
 HOME_DIR = os.path.expanduser('~')
 FS_DOTFILES_PATH = os.path.join(HOME_DIR, 'dotfiles')
 EMACS_LIBS_DIR = os.path.join(HOME_DIR, '.emacs.d')
+BACKUP_DIR = os.path.join(HOME_DIR, 'old-dotfiles.backup')
 
 DOTFILES_TO_IGNORE = frozenset(['.gitignore', '.git'])
 
 def setup_dotfiles():
     logging.info('Setting up dotfiles')
+    # Create backup directory if needed
+    if not os.path.exists(BACKUP_DIR):
+        os.makedirs(BACKUP_DIR)
+        
     for file_name in os.listdir(FS_DOTFILES_PATH):
         item_path = os.path.join(FS_DOTFILES_PATH, file_name)
         if file_name.startswith('.') and file_name not in DOTFILES_TO_IGNORE:
             # Must be a dotfile. Create symlink
             target_symlink_path = os.path.join(HOME_DIR, file_name)
             if os.path.exists(target_symlink_path):
+                shutil.move(target_symlink_path, os.path.join(BACKUP_DIR, target_symlink_path))
+                '''
                 if os.path.islink(target_symlink_path):
                     os.unlink(target_symlink_path)
                 else:
                     shutil.rmtree(target_symlink_path) if os.path.isdir(target_symlink_path) else os.remove(target_symlink_path)
+                '''
             try:
                 os.symlink(item_path, target_symlink_path)
             except OSError as e:
@@ -82,7 +90,7 @@ def main(args):
         setup_git()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser('Bootstrap dotfiles')
+    parser = argparse.ArgumentParser(description='Bootstrap dotfiles')
     parser.add_argument('--l', action='store_false', default=True, help='Dump logging to stdout/stderr')
     parser.add_argument('--a', action='store_true', default=False, help='Don\'t setup all')
     parser.add_argument('--d', action='store_true', default=False, help='Explicit setup dotfiles')
