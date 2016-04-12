a5e2c433502c81a38889fe48ca4549163d67705a
Gaurav Chauhan <iitb.gaurav@gmail.com>
2016-04-12 14:02:43 +0530

    update exports

--- .bashrc.d/exports.bash
+++ .bashrc.d/exports.bash
@@ -5,11 +5,9 @@ if [ $TERM = "screen" ]; then echo -ne "\033k${USER}@${HOSTNAME}\033\\"; fi;
 # User specific aliases and functions
 export EDITOR=emacs
 export VISUAL=emacs
-export ANDROID_NDK=~/android-ndk-r10e
-export PATH=$PATH:$HOME/bin:/sbin:/usr/sbin:/usr/local/mysql/bin:/usr/local/bin:/usr/share/phabricator/arcanist/bin:$HOME/code/setup/dotfiles/bin
+export PATH=$PATH:$HOME/bin
 export ROOT=$HOME/code
-export PYTHONPATH=$PYTHONPATH:$ROOT/python:$ROOT/python/stubs:.
-export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64
+#export PYTHONPATH=$PYTHONPATH:$ROOT/python:$ROOT/python/stubs:.
 export AWS_CONFIG_FILE=$HOME/.s3cfg
 export GOPATH=$ROOT/go
 export GOROOT=/usr/lib/go
